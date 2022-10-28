49d0ba32b6f27cc18055d457533c375da1fbb645
alansubedi123 <alansubedi123@gmail.com>
2022-10-28 12:35:36 -0500

    Modified gitignore

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ from ISS_LIS import add_isslis_path
 from dotenv import load_dotenv
 import os
 
+##LOAD_ENV_VARIABLES
+print("Loading env variables...." , end='')
 load_dotenv()
 TRMM_LIS_S3_PATH=os.getenv('TRMM_LIS_BASE_PATH')
 OTD_S3_PATH=os.getenv('OTD_BASE_PATH')
 ISS_LIS_S3_PATH=os.getenv('ISS_LIS_SPRING2023_BASE_PATH')
+print("Complete")
 
+##DECLARE_INITIALIZE 
+print("Initializing variables....", end='')
 driver_name = "raster2.sqlite"
 rasters = {}
+print("Complete")
 
-print("Step 1 Complete!!")
-############################################MAKE_RASTERS_HERE##########################################################################################
-
+##ADD_TO_RASTER_DICTIONARY_HERE
+print("Creating Raster Dictionary....", end='')
 TRMM_LIS = add_trmmlis_path(rasters, TRMM_LIS_S3_PATH)
 OTD = add_otd_path(rasters, OTD_S3_PATH)
 ISS_LIS = add_isslis_path(rasters, ISS_LIS_S3_PATH)
+print("Complete")
 
-print("Step 2 Complete!!")
-#############################################ADD_PATH_TO_SQLITE_DB_HERE################################################################################
+##ADD_PATH_TO_SQLITE_DB_HERE
+print("Adding paths to sqlitedb....", end='')
 driver = sqlitedbb(driver_name, rasters)
 driver.make_new_db()
+print("Complete")
 
-print("Step 3 Complete!!")
-##############################################PRITN_DRIVER_HERE########################################################################################
+##PRITN_DRIVER_HERE
 driver.print()
-#######################################################################################################################################################
+
+
